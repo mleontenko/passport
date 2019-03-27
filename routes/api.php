fb93cdf1421dec09820bf773708ce7ebb5806cf4
Marin Leontenko <marin.leontenko@gmail.com>
2019-03-27 10:13:11 +0100

    api za registraciju/prijavu sa klijentskom aplikacijom

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| API routes for auth
+|--------------------------------------------------------------------------
+*/
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
